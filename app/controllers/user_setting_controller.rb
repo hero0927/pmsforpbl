059182c3ab17d2c36816bdb563332833c5ddc319
chengandpeng <chengandpeng@msn.com>
2012-02-12 18:40:26 +0900

    2012/02/12 18:40

--- app/controllers/user_setting_controller.rb
+++ app/controllers/user_setting_controller.rb
@@ -1,28 +1,35 @@
+# coding: utf-8
 class UserSettingController < ApplicationController
+  before_filter :require_user
   def index
-      @project = @current_user
-    render :text => @current_user
+
   end
 
   def Update
-    @project = @current_user
-    render :text => @current_user
-    # @project.password = params[:password]
-    # @project.project_name = params[:project_name]
-    # @project.team_name = params[:team_name]
-    # start = Date.new(params[:start][:year].to_i, params[:start][:month].to_i, params[:start][:day].to_i)
-    # completion = Date.new(params[:completion][:year].to_i, params[:completion][:month].to_i, params[:completion][:day].to_i)
-    # stats = Date.new(params[:stats][:year].to_i, params[:stats][:month].to_i, params[:stats][:day].to_i)
-    # @project.project_start_date = start
-    # @project.project_completion_date = completion
-    # @project.project_stats_start_date = stats
-    # @project.project_state = params[:project_state]
-    # respond_to do |format|
-      # if   @project.save
-        # format.html {redirect_to :back}
-      # else
-        # format.json {render json: @test.errors, status: :unprocessable_entity}
-      # end
-    # end
+    if @current_user.type == 'Project'
+      @current_user.password = params[:password]
+      @current_user.password_confirmation = params[:password_confirmation]
+      @current_user.project_name = params[:project_name]
+      @current_user.team_name = params[:team_name]
+      start = Date.new(params[:start][:year].to_i, params[:start][:month].to_i, params[:start][:day].to_i)
+      completion = Date.new(params[:completion][:year].to_i, params[:completion][:month].to_i, params[:completion][:day].to_i)
+      stats = Date.new(params[:stats][:year].to_i, params[:stats][:month].to_i, params[:stats][:day].to_i)
+      @current_user.project_start_date = start
+      @current_user.project_completion_date = completion
+      @current_user.project_stats_start_date = stats
+      @current_user.project_state = params[:project_state]
+    else
+      @current_user.password = params[:password]
+      @current_user.password_confirmation = params[:password_confirmation]
+    end
+    respond_to do |format|
+      if   @current_user.save
+        flash[:success] = "変更成功"
+        format.html {redirect_to :back}
+      else
+        flash[:password_errors] = "同じパスワードを入力してください"
+        format.html {redirect_to :action => 'index' }
+      end
+    end
   end
 end
